c02495e6c9ca811b7f3d8608f27a48ad738772b1
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-07-19 15:03:42 +0900

    add tables expense and conv

---
+++ backend/src/models/database/conversation.py
@@ -0,0 +1,25 @@
+from sqlalchemy import VARCHAR, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import BaseTableModel
+from .graph import Graph
+from .user import User
+
+
+class Conversation(BaseTableModel):
+    __tablename__ = "conversation"
+
+    graphId: Mapped[str] = mapped_column(
+        VARCHAR(255),
+        ForeignKey(Graph.id),
+        nullable=False,
+        index=True,
+    )
+    userId: Mapped[str] = mapped_column(
+        VARCHAR(255),
+        ForeignKey(User.id),
+        nullable=True,
+        index=True,
+        default_factory=lambda: None,
+        server_default=None,
+    )
