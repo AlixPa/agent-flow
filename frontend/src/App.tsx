780bf8741faeac567601eaf082db6235e4999f2a
ohayo-u <hskwhrk.ooaaaua.924@gmail.com>
2025-07-27 18:48:22 +0900

    feat: create tabs UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import { Button } from "@/components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+
+type Tab = "flow" | "chat";
 
 function App() {
+  const [tab, setTab] = useState<Tab>("flow");
+
+  const handleTabChange = (v: string) => {
+    setTab(v as Tab)
+  }
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
-  )
+    <Tabs defaultValue="flow" className="w-[400px]" value={tab} onValueChange={handleTabChange}>
+      <TabsList>
+        <TabsTrigger value="flow">Flow</TabsTrigger>
+        <TabsTrigger value="chat">Chat</TabsTrigger>
+      </TabsList>
+      <TabsContent value="flow">
+        Flow tab
+      </TabsContent>
+      <TabsContent value="chat">Chat tab</TabsContent>
+    </Tabs>
+  );
 }
 
-export default App
+export default App;
