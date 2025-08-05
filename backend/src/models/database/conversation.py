4df322d87f8a1e0dc4d75ce099f42db08c56f8de
AlixPa <81264144+AlixPa@users.noreply.github.com>
2025-08-05 21:18:26 +0900

    add trace and agent name for logfire

--- backend/src/models/database/conversation.py
+++ backend/src/models/database/conversation.py
@@ -15,11 +15,18 @@ class Conversation(BaseTableModel):
         nullable=False,
         index=True,
     )
+    traceParent: Mapped[str] = mapped_column(
+        VARCHAR(255),
+        nullable=False,
+        # TODO: remove this once all db backfilled
+        server_default="default",
+    )
     userId: Mapped[str | None] = mapped_column(
         VARCHAR(255),
         ForeignKey(User.id),
         nullable=True,
         index=True,
+        # TODO: remove this once user system is decided and all db backfilled
         default_factory=lambda: None,
         server_default=None,
     )
