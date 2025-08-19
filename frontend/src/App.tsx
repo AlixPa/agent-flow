b54a0d2a56024b807b464f281ad74384b03337e5
ohayo-u <hskwhrk.ooaaaua.924@gmail.com>
2025-08-19 09:31:39 +0900

    feat: create graph details page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,17 @@ import "@xyflow/react/dist/style.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { CreateGraphScreen } from "./pages/create-graph";
 import GraphList from "./pages/graph-list";
+import GraphDetails from "./pages/graph-details";
 
 function App() {
   return (
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<GraphList />} />
         <Route path="/create" element={<CreateGraphScreen />} />
+        <Route path="graph">
+          <Route path=":graph" element={<GraphDetails />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/graph-details.tsx
@@ -0,0 +1,44 @@
+import "@xyflow/react/dist/style.css";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
+import "@xyflow/react/dist/style.css";
+import { ReactFlowProvider } from "@xyflow/react";
+import { DnDProvider } from "@/contexts/dnd-context";
+import { GraphTab } from "@/widgets/graph-tab";
+import { ConversationTab } from "@/widgets/conversation-tab";
+
+type Tab = "graph" | "conversation";
+
+function GraphDetails() {
+  const [tab, setTab] = useState<Tab>("graph");
+
+  const handleTabChange = (v: string) => {
+    setTab(v as Tab);
+  };
+
+  return (
+    <Tabs
+      defaultValue="graph"
+      value={tab}
+      onValueChange={handleTabChange}
+      className="h-screen"
+    >
+      <TabsList>
+        <TabsTrigger value="graph">Graph</TabsTrigger>
+        <TabsTrigger value="conversation">Conversation</TabsTrigger>
+      </TabsList>
+      <TabsContent value="graph">
+        <ReactFlowProvider>
+          <DnDProvider>
+            <GraphTab />
+          </DnDProvider>
+        </ReactFlowProvider>
+      </TabsContent>
+      <TabsContent value="conversation">
+        <ConversationTab />
+      </TabsContent>
+    </Tabs>
+  );
+}
+
+export default GraphDetails;
